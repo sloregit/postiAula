7fa68f79cdebfc35afd3009fb0c7918a603aaad1
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-07-14 18:35:39 +0200

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,24 @@ import { StudentiDBserviceService } from './studenti-dbservice.service';
 import { CercaStudenteComponent } from './cerca-studente/cerca-studente.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CercaSezioneComponent } from './cerca-sezione/cerca-sezione.component';
+//Angular Material
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+  ],
   declarations: [AppComponent, CercaStudenteComponent, CercaSezioneComponent],
   bootstrap: [AppComponent, CercaStudenteComponent],
   providers: [StudentiDBserviceService],
