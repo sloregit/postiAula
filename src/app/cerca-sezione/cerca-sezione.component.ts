16231107d0f8dfa4287f4f6f0bb33d6d8737bb0a
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-07-14 16:09:53 +0200

    2 files modified

--- src/app/cerca-sezione/cerca-sezione.component.ts
+++ src/app/cerca-sezione/cerca-sezione.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentiDBserviceService } from '../studenti-dbservice.service';
-
+export class Studente {
+  nome: string;
+  sezione: string;
+  constructor(nome, sezione) {
+    this.nome = nome;
+    this.sezione = sezione;
+  }
+}
+export class Aula {
+  file;
+  posti;
+}
 @Component({
   selector: 'app-cerca-sezione',
   templateUrl: './cerca-sezione.component.html',
@@ -8,10 +19,25 @@ import { StudentiDBserviceService } from '../studenti-dbservice.service';
 })
 export class CercaSezioneComponent implements OnInit {
   arraySezioni: Array<string>;
-  sezione: string;
+  sezioneResponse: Array<Studente>;
+  studenti = [];
   constructor(private http: StudentiDBserviceService) {
     this.arraySezioni = ['A', 'B', 'C', 'D', 'E', 'F'];
   }
-
+  cercaSezione(sezione) {
+    this.studenti = [];
+    this.http.getSezione(sezione).subscribe({
+      next: (res) => {
+        this.sezioneResponse = JSON.parse(res);
+        this.sezioneResponse.map((studente) => {
+          this.studenti.push(new Studente(studente.nome, studente.sezione));
+          console.log(this.studenti);
+        });
+      },
+      error: (e) => {
+        console.error(e);
+      },
+    });
+  }
   ngOnInit() {}
 }
