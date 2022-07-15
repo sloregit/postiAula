5f4b3885a78470d7bd06bfaafa6cf4b7b5221aa7
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-07-16 01:54:46 +0200

    2 files modified

--- src/app/aggiungi-classe/aggiungi-classe.component.ts
+++ src/app/aggiungi-classe/aggiungi-classe.component.ts
@@ -10,15 +10,27 @@ import { StudentiDBserviceService } from '../studenti-dbservice.service';
 export class AggiungiClasseComponent implements OnInit {
   panelOpenState = false;
   arrayStudenti: Array<Studente>;
+  selezionati: Array<Studente>;
+  apriLista: boolean;
   constructor(private http: StudentiDBserviceService) {
     this.arrayStudenti = [];
+    this.apriLista = false;
+  }
+  chiudi() {
+    this.apriLista = false;
   }
   foo() {
     this.http.cercaTutti().subscribe((val) => {
       const collection = JSON.parse(val);
       collection.map((studente) => this.arrayStudenti.push(studente));
-      console.log(this.arrayStudenti);
     });
+    this.apriLista = true;
+  }
+  selezionatiUpdate(list) {
+    this.selezionati = list;
+  }
+  foo3() {
+    console.log(this.selezionati);
   }
   ngOnInit() {}
 }
