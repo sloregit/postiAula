a63c1dcaaa787d704b7b065f1c304e03ecafccac
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-07-15 13:40:31 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,8 +20,8 @@ import { MatMenuModule } from '@angular/material/menu';
 import { AggiungiStudenteComponent } from './aggiungi-studente/aggiungi-studente.component';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-import { MAT_DATE_LOCALE } from '@angular/material/core';
- 
+//import { MAT_DATE_LOCALE } from '@angular/material/core';
+
 @NgModule({
   imports: [
     BrowserModule,
@@ -49,7 +49,7 @@ import { MAT_DATE_LOCALE } from '@angular/material/core';
   bootstrap: [AppComponent, CercaStudenteComponent],
   providers: [
     StudentiDBserviceService,
-    { provide: MAT_DATE_LOCALE, useValue: 'it-IT' },
+    //{ provide: MAT_DATE_LOCALE, useValue: 'it-IT' },
   ],
 })
 export class AppModule {}
