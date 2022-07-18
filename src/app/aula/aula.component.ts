e9c53aa5a32890b26974feff05f955d7d53e8552
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-07-18 03:12:28 +0200

    2 files modified

--- src/app/aula/aula.component.ts
+++ src/app/aula/aula.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CercaAuleService } from './cerca-aule.service';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { Studente } from '../shared-class';
 @Component({
   selector: 'app-aula',
   templateUrl: './aula.component.html',
@@ -10,11 +11,13 @@ export class AulaComponent implements OnInit {
   @Input() arraySezioni: Array<string>;
   @Input() arrayAnniScolastici: Array<number>;
   risposta;
+  arrayStudenti: Array<Studente>;
   constructor(private cerca: CercaAuleService) {}
   cercaAula() {
     this.cerca.cercaAula(3, 'D').subscribe((val) => {
       console.log(val);
-      this.risposta = val;
+      this.risposta = JSON.parse(val);
+      this.arrayStudenti = this.risposta.classe;
     });
   }
   ngOnInit() {}
