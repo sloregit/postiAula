4170acaee6a3d7dfad442ec5f64f5c077fdcb7c2
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-07-20 22:18:08 +0200

    3 files modified

--- src/app/aula/aula.component.ts
+++ src/app/aula/aula.component.ts
@@ -7,13 +7,38 @@ import {
   moveItemInArray,
   transferArrayItem,
 } from '@angular/cdk/drag-drop';
-export class aulaV {
-  classe: Classe;
-  constructor(classe) {
-    this.classe = classe;
+import { switchScan } from 'rxjs';
+
+export class aulaV implements Classe {
+  anno;
+  sezione;
+  classe;
+  constructor(response) {
+    this.anno = response.anno;
+    this.sezione = response.sezione;
+    this.classe = response.classe;
   }
   genera() {
-    
+    try {
+      if (this.classe.length) {
+        switch (this.classe.length) {
+          case this.classe.length >= 6: {
+          }
+          case this.classe.length > 6 && this.classe.length <= 12: {
+          }
+          case this.classe.length > 12 && this.classe.length <= 18: {
+          }
+          case this.classe.length > 18 && this.classe.length <= 24: {
+          }
+          case this.classe.length > 24 && this.classe.length <= 30: {
+          }
+          case this.classe > 30: {
+          }
+        }
+      }
+    } catch (e) {
+      ('Errore in aulaV.genera');
+    }
   }
 }
 
@@ -26,14 +51,18 @@ export class AulaComponent implements OnInit {
   @Input() arraySezioni: Array<string>;
   @Input() arrayAnniScolastici: Array<number>;
   risposta;
+  aula: aulaV;
   arrayStudenti: Array<Studente>;
   arrayStudenti2: Array<Studente>;
   arrayStudenti3: Array<Studente>;
+  arr;
   constructor(private cerca: CercaAuleService) {}
   cercaAula(anno, sezione) {
-    this.cerca.cercaAula(+anno, sezione).subscribe((val) => {
+    this.cerca.cercaAula(1, 'D').subscribe((val) => {
       this.risposta = JSON.parse(val);
-      console.log(this.risposta);
+      console.log(val);
+      this.aula = new aulaV(this.risposta);
+      console.log(this.aula.classe);
       this.arrayStudenti = this.risposta.classe.slice(0, 4);
       this.arrayStudenti2 = this.risposta.classe.slice(4, 9);
       this.arrayStudenti3 = this.risposta.classe.slice(10);
