81e7b200647cabe57e518b08faaf1ffb4381458c
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-07-24 12:28:31 +0200

    2 files modified

--- src/app/aula/aula.component.ts
+++ src/app/aula/aula.component.ts
@@ -7,44 +7,38 @@ import {
   moveItemInArray,
   transferArrayItem,
 } from '@angular/cdk/drag-drop';
-import { switchScan } from 'rxjs';
 
+export interface aula {
+  file: Array<Studente>;
+}
 @Component({
   selector: 'app-aula',
   templateUrl: './aula.component.html',
   styleUrls: ['./aula.component.css'],
 })
-export class AulaComponent implements OnInit {
+export class AulaComponent implements OnInit, aula {
   @Input() arraySezioni: Array<string>;
   @Input() arrayAnniScolastici: Array<number>;
   risposta: Classe;
-  aula;
+  file;
   constructor(private service: CercaAuleService) {
-    this.aula = [];
+    this.file = [];
   }
 
   cercaAula(anno, sezione) {
     this.service.cercaAula(1, 'D').subscribe((val) => {
       this.risposta = JSON.parse(val);
       this.risposta.classe.map((fila, i) => {
-        this.aula[i] = fila;
+        this.file[i] = fila;
       });
-      console.log(this.aula);
-      /*
-      this.arrayStudenti = this.risposta.classe.slice(0, 5);
-      this.arrayStudenti2 = this.risposta.classe.slice(5, 10);
-      this.arrayStudenti3 = this.risposta.classe.slice(10, 15);
-      this.arrayStudenti4 = this.risposta.classe.slice(15, 20);
-      this.arrayStudenti5 = this.risposta.classe.slice(20, 25);*/
     });
   }
 
   foo() {
-    console.log(this.aula);
     let _classe = new Classe(
       this.risposta.anno,
       this.risposta.sezione,
-      this.aula
+      this.file
     );
     this.service.aggiornaAula(_classe).subscribe((res) => {
       console.log(res);
@@ -62,7 +56,7 @@ export class AulaComponent implements OnInit {
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
-        event.currentIndex
+        event.targetIndex
       );
     }
   }
